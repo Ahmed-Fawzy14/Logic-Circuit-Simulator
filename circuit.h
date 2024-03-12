1bbbc575a8118fccd546e6b06498980e6739051f
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2024-03-12 23:52:20 +0200

    Update circuit.h

--- circuit.h
+++ circuit.h
@@ -5,13 +5,17 @@
 #ifndef LOGIC_CIRCUIT_SIMULATOR_CIRCUIT_H
 #define LOGIC_CIRCUIT_SIMULATOR_CIRCUIT_H
 #include "Logic_Gate.h"
+#include <queue>
 
 class circuit {
 private:
 
     //This has all the circuit components
     //Traverse over allGates to fill this
+    //Use the vector for flagging method 
     vector<Logic_Gate> usedGates;
+    //Use the Queue for popping method 
+    queue<Logic_Gate> usedGates;
 
     //This is the worst case propogation delay in ps
     int propgration_Delay_ps;
