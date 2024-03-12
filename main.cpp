8aa76f346ca0b2d6cf53129ce25547ae0648d820
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2024-03-12 22:34:14 +0200

    Updated main.cpp with both codes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,54 @@
 #include "Logic_Gate.h"
 using namespace std;
 
-int main() {
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Logic_Gate.cpp"
+using namespace std;
 
+void read_lib_file()
+{
+    ifstream input;
+    string line, name, no_of_inputs, delay, expression;
+    input.open("C:/Users/Power/Desktop/Project dd1/INV, 1, ~(i1), 50.txt");
+    vector <Logic_Gate> v;
+    int j = 0;
+    while (getline(input, line)) 
+    {
+
+    istringstream subst(line);
+    getline(subst, name, ',');
+    getline(subst, no_of_inputs, ',');
+    getline(subst, expression, ',');
+    getline(subst, delay, ',');
+    vector <string> Expressions(expression.length());
+    for (int i = 0; i < expression.length(); i++)
+    {
+        Expressions[i] = expression[i];
+    }
+    Logic_Gate* gate= new Logic_Gate(name, stoi(no_of_inputs), stoi(delay), Expressions);
+    v.push_back(*gate);
+    }    
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] .getName() << " " << v[i] .getNumOfInputs() << " " << v[i].getDelayPs() << " ";
+        for (int i = 0; i < v[i].getExpression().size(); i++)
+        {
+            cout << v[i].(*(getexpression()+i));
+        }
+    }
+ 
+}
+
+int main()
+{
+//Abdeen's Read File
+    read_lib_file();
+//Code from Fawzy's Main
+    
     vector<string> testExp = {"A1", "&", "A2"};
 
     Logic_Gate test_Obj("test1", 2, 500, testExp);
@@ -29,3 +75,5 @@ int main() {
     }
     return 0;
 }
+
+
