62ffad394298a54450136e7a915ced16ba53b856
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2024-03-14 20:53:50 +0200

    Added implementation to circuit.cpp

--- circuit.cpp
+++ circuit.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "circuit.h"
+
+ bool circuit::Operator(int index,  vector<pair<string,int>> cir_Input_Names)
+    {
+        Logic_Gate gate = usedGates[index];
+
+        vector<string> exp = gate.getExpression();
+
+        //maps the value to its input var
+        unordered_map<string, pair<string,int>> map;
+
+        int i =0;
+        for(auto x : exp)
+        {
+            if(i%2 == 0)
+            {
+                map[x] = cir_Input_Names[i];
+                i++;
+            }
+            else
+            {
+
+                if(x == "&")
+                    map[x] = make_pair("&", -2);
+                //add the rest of the operators
+            }
+
+        }
+
+        int j =0;
+        bool output = 0;
+        for(int j = 0; j<map.size(); j++)
+        {
+            string i1 = exp[j];
+            string i2{};
+
+            if(j%2 != 0)
+            {
+                if((map[i1].second).first == "&")
+                    output = output & ((y.second).second & (y.second).second);
+            }
+        }
+
+
+        return 0;
+    }
