8877ba03fb2c7d045eddb54b7ee9243cea161550
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2024-03-14 23:56:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
-#include <fstream> // Including the fstream library to read files
-#include <iostream> // Including the iostream library in case of couting any output for debugging or clarity purposes
-#include <string> // Including the string library to use string functions
-#include <sstream> // Including the sstream library to store data from files into variables using delimiters
-#include "circuit.cpp" // Including the circuit.cpp file to create an object for each circuit
-#include "Logic_Gate.cpp" // Including the Logic_Gate.cpp file to create an object for each gate in the lib file
-#include "unordered_map" // Including the unordered_map class to be used in the storing of gates
+#include "program.cpp"
 using namespace std;
 
 
 int main()
 {
 
-///////Testing
+    //Reading all the gates from the .lib file
     vector <Logic_Gate> all_gates = read_lib_file();
-    
+
+//Reading the .cir file
+    ifstream inputfile;
+    inputfile.open(".cir_file"); // opening the file
+    readfile(inputfile);
+
+    // 2d vector delaration
+    //Combining data from .cir and .lib only to the gates used
+    vector<vector<string>> components;
+    fillvector(components, inputfile);
+
+    objectmodification(components);
+
+
+    //Testing each ciruit 
     circuit c1;
     c1.read_lib_file();
     c1.readfile();
@@ -38,15 +46,6 @@ int main()
     c6.read_lib_file();
     c6.readfile();
     c6.run();
-ifstream inputfile;
-    inputfile.open(".cir_file"); // opening the file
-    readfile(inputfile);
-
-    // 2d vector delaration
-    vector<vector<string>> components;
-    fillvector(components, inputfile);
-
-    objectmodification(components);
 
     
     return 0;
