e3a17707e98512711052637bd8a1616b25edf85b
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2024-03-29 04:35:19 +0200

    Update circuit.h

--- circuit.h
+++ circuit.h
@@ -10,104 +10,130 @@
 #include <iostream>
 using namespace std;
 
-class circuit
-{
+class circuit {
 private:
-    // This has all the circuit components
-    // Traverse over allGates to fill this
-    // Use the vector for flagging method
-    vector<Logic_Gate> usedGates;
-    // Use the Queue for popping method
-    //  queue<Logic_Gate> usedGates;
 
-    // This is the worst case propagation delay in ps
+    //This has all the circuit components
+    //Traverse over allGates to fill this
+    //Use the vector for flagging method
+    vector<Logic_Gate> usedGates;
+
+    vector<shared_ptr<Logic_Gate>> usedGatesPtr;
+
+    //This is the worst case propogation delay in ps
     int propgration_Delay_ps;
 
-    // This is the final time in the .stim file
+    //This is the final time in the .stim file
     int time_End;
 
     vector <tuple<string, bool, int>> cirInputs;
     map <string, tuple <string, bool, int>> current_Inputs_Map;
     map <string, Logic_Gate>  All_gates;
 
+
+
+
 public:
+
+
+
     void printLogicGateContents();
 
-    int usedGatessize()
-    {
+
+
+    int usedGatessize(){
         return usedGates.size();
     }
 
     void Run();
 
-    void set_All_gates(map<string, Logic_Gate> Library_gates)
+    //Send the index of the object in usedGates to this function and the current cir_Input_Names
+    //bool Operator(int index, vector<tuple <string, bool, int>> current_Inputs);
+
+    void updateUsedGatesPtr() {
+        usedGatesPtr.clear(); // Clear existing shared pointers
+        for (auto& gate : usedGates) {
+            usedGatesPtr.push_back(make_shared<Logic_Gate>(gate));
+        }
+    }
+
+    // Getter for usedGatesPtr
+    vector<shared_ptr<Logic_Gate>>& getUsedGatesPtr() {
+        return usedGatesPtr;
+    }
+
+    void set_All_gates(map <string, Logic_Gate> Library_gates)
     {
         All_gates = Library_gates;
     }
 
-    map<string, Logic_Gate> get_All_gates()
+    map <string, Logic_Gate> get_All_gates()
     {
         return All_gates;
     }
 
-    void setcurrent_Inputs_Map(map<string, tuple<string, bool, int>> current_Inputs_Map)
+    void setcurrent_Inputs_Map(map<string, tuple <string, bool, int>> current_Inputs_Map)
     {
         this->current_Inputs_Map = current_Inputs_Map;
     }
 
-    map<string, tuple<string, bool, int>> getcurrent_Inputs_Map()
+    map<string, tuple <string, bool, int>> getcurrent_Inputs_Map()
     {
         return current_Inputs_Map;
     }
 
-    map<string, tuple<string, bool, int>> &getcurrent_Inputs_MapREF()
+    map<string, tuple <string, bool, int>>& getcurrent_Inputs_MapREF()
     {
-        return this->current_Inputs_Map;
+        return this-> current_Inputs_Map;
     }
 
-    vector<tuple<string, bool, int>> &getcirInputsREF()
+
+    vector< tuple <string, bool, int>>& getcirInputsREF()
     {
-        return this->cirInputs;
+        return this-> cirInputs;
     }
 
-    void setusedGates(vector<Logic_Gate> gates)
+    void setusedGates(vector <Logic_Gate> gates)
     {
         usedGates = gates;
     }
 
-    void pushgate_in_usedGates(Logic_Gate gate)
-    {
+    void pushgate_in_usedGates(Logic_Gate gate){
         usedGates.push_back(gate);
     }
 
-    void setcirInputs(vector<tuple<string, bool, int>> &inputs)
+    void setcirInputs(vector <tuple <string, bool, int>>& inputs)
     {
         cirInputs = inputs;
     }
-
     void objectmodification(vector<vector<string>> &components);
     void fillvector(vector<vector<string>> &components, string fileName);
 
-    vector<Logic_Gate> getusedGates()
+    vector <Logic_Gate> getusedGates()
     {
         return usedGates;
     }
 
-    vector<Logic_Gate> &getREF_usedGates()
+    vector <Logic_Gate>& getREF_usedGates()
     {
         return this->usedGates;
     }
 
-    vector<tuple<string, bool, int>> getcirInputs()
+    vector< tuple <string, bool, int>> getcirInputs()
     {
         return cirInputs;
     }
 
-    void read_lib_file(const string &filepath, string fileName); // function to read lib file
+    void read_lib_file(const string& filepath, string fileName); // function to read lib file
     void write_to_sim(string fileName);
-    void read_stim_file(const string &filepath);
+    void read_stim_file(const string& filepath);
 
+    //  void objectmodification(vector<vector<string>> &components);
+    // void read_circ_file(ifstream &inputfile);
+    // void fillvector(vector<vector<string>> &components,  ifstream &inputfile);
     void sort_Cir_inputs();
+
 };
 
-#endif // LOGIC_CIRCUIT_SIMULATOR_CIRCUIT_H
+
+#endif //LOGIC_CIRCUIT_SIMULATOR_CIRCUIT_H
